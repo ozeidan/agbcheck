6a50ff4e509fe80fa449925b7e6dcbc4ce39c76b
TobiasTEndres <tobias.t.endres@deutschebahn.com>
2017-04-29 16:17:16 +0200

    Ein kleiner ES Client in Python

---
+++ esClient.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from elasticsearch import Elasticsearch
+
+es = Elasticsearch(
+    ['first-cluster-340082074.eu-west-1.bonsaisearch.net'],
+    http_auth=('jan3osgggf', 'tvept1eq65'),
+    port=443,
+    use_ssl=True,
+    verify_certs=False,
+)
+
+doc = {
+    'author': 'kimchy',
+    'text': 'Elasticsearch: cool. bonsai cool.',
+    'timestamp': datetime.now(),
+}
+
+res = es.index(index="agbcheck", doc_type='agb',  body=doc)
+print(res['created'])
+
+res = es.get(index="agbcheck", doc_type='agb', id=1)
+print(res['_source'])
+
+es.indices.refresh(index="agbcheck")
+
+res = es.search(index="agbcheck", body={"query": {"match_all": {}}})
+print("Got %d Hits:" % res['hits']['total'])
+for hit in res['hits']['hits']:
+    print("%(timestamp)s %(author)s: %(text)s" % hit["_source"])
